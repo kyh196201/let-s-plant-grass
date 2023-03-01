96facb9ef54ca6bf8cdf004cf429b1803f1f7811
seungwoo Kim <kyh196201@gmail.com>
2023-03-02 00:44:17 +0900

    lib에 있는 github 파일 utils로 이동

---
+++ src/utils/github.ts
@@ -0,0 +1,35 @@
+import Commit from '@/interfaces/commit';
+import type { GithubEvent } from '@/api';
+
+/**
+ * 깃허브 이벤트 push 이벤트 타입인지 여부 반환
+ *
+ * @param eventResponse 깃허브 이벤트 response
+ * @returns
+ */
+export const isPushEvent = function isPushEvent(eventResponse: GithubEvent) {
+  return eventResponse.type === 'PushEvent';
+};
+
+/**
+ * 사용자가 작성한 커밋 목록을 반환
+ *
+ * @param eventResponses 깃허브 사용자 events api를 통해 조회한 response
+ * @returns 사용자가 작성한 커밋 목록
+ */
+export const getCommitsFromResponse = function getCommitsFromResponse(eventResponses: GithubEvent[]): Commit[] {
+  const newCommits: Commit[] = [];
+
+  eventResponses.forEach((event) => {
+    const { commits } = event.payload;
+
+    commits.forEach((commit) => {
+      const newCommit = new Commit(event.created_at, commit);
+      newCommits.push(newCommit);
+    });
+  });
+
+  return newCommits;
+};
+
+export default {};
