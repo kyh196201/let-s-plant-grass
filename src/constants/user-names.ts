5c2e1dde3b297711399fd6b31b8f9d4b8863f3ac
seungwoo Kim <kyh196201@gmail.com>
2023-04-04 00:19:50 +0900

    사용자 추가

--- src/constants/user-names.ts
+++ src/constants/user-names.ts
@@ -1,3 +1,3 @@
-const USER_NAMES: string[] = ['kyh196201', 'JEONMINJU', 'teller2016', 'JOANNASHIN'];
+const USER_NAMES: string[] = ['kyh196201', 'JEONMINJU', 'teller2016', 'JOANNASHIN', 'limEnough'];
 
 export default USER_NAMES;
